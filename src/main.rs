10d692e32a22537d691abdfbdc80bc9b605d60bc
elasticspoon <mail@yuribocharov.dev>
2025-10-05 20:31:20 -0400

    feat: add some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use std::net::TcpListener;
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::{env, net::TcpListener, process};
 
 fn main() {
-    let listener = TcpListener::bind("localhost:3000").expect("failed to bind to port");
+    // let port = env::var("PORT").map_or_else(|_| 3000, |port| port.parse::<i32>().unwrap_or(3000));
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|port| port.parse::<i32>().ok())
+        .unwrap_or(3000);
+    let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 3000));
+    let listener = TcpListener::bind(addr).unwrap_or_else(|err| {
+        eprintln!("Failed to bind to localhost:{}: {}", port, err);
+        process::exit(1);
+    });
 
+    println!("Listening on {}", listener.local_addr().unwrap());
     for conn in listener.incoming() {
         match conn {
-            Ok(_) => println!("accepted connection"),
+            Ok(_) => println!("Accepted connection"),
             Err(err) => println!("failled to accept connection: {:?}", err),
         }
     }
