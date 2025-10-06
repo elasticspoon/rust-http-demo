d58702acdfdc539e82c051655315874f3851d6ee
elasticspoon <mail@yuribocharov.dev>
2025-10-05 23:23:16 -0400

    feat: basic html write

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::io::{BufRead, BufReader, Write};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream};
 use std::{env, net::TcpListener, process};
 
 fn main() {
@@ -16,8 +17,27 @@ fn main() {
     println!("Listening on {}", listener.local_addr().unwrap());
     for conn in listener.incoming() {
         match conn {
-            Ok(_) => println!("Accepted connection"),
+            Ok(stream) => {
+                println!("Accepted connection");
+                handle_connection(stream);
+            }
             Err(err) => println!("failled to accept connection: {:?}", err),
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
