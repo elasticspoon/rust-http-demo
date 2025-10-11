cf026cdd23abad01042c032e7790d2057731c915
elasticspoon <mail@yuribocharov.dev>
2025-10-10 20:46:36 -0400

    feat: add some parsing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fmt::{self, Display};
 use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream};
+use std::str::FromStr;
 use std::{env, net::TcpListener, process};
 
 fn main() {
@@ -32,16 +33,100 @@ fn main() {
 struct HttpRequest {
     verb: HttpVerb,
     protocol: HttpProtocol,
+    path: String,
     headers: HashMap<String, String>,
+    body: Option<String>,
+}
+
+impl HttpRequest {
+    fn build(request: Vec<String>) -> Result<HttpRequest, String> {
+        let header: Vec<&str> = request
+            .first()
+            .expect("should container a header")
+            .split(" ")
+            .collect();
+        if let (Some(verb), Some(path), Some(protocol)) =
+            (header.first(), header.get(1), header.get(2))
+        {
+            let path = (**path).to_string();
+            let verb = HttpVerb::from_str(verb)?;
+            let protocol = HttpProtocol::from_str(protocol)?;
+            Ok(HttpRequest {
+                verb,
+                path,
+                protocol,
+                headers: HashMap::new(),
+                body: None,
+            })
+        } else {
+            Err(format!("invalid header: {:#?}", header))
+        }
+    }
+}
+
+impl Display for HttpRequest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{verb} {path} {protocol}\r\n\r\n",
+            verb = self.verb,
+            path = self.path,
+            protocol = self.protocol,
+        )
+    }
 }
 
 enum HttpProtocol {
     OnePointOne,
 }
+
+impl FromStr for HttpProtocol {
+    type Err = String;
+
+    fn from_str(protocol: &str) -> Result<Self, Self::Err> {
+        match protocol {
+            "HTTP/1.1" => Ok(HttpProtocol::OnePointOne),
+            _ => Err(format!("invalid HttpProtocol: {}", protocol)),
+        }
+    }
+}
+
+impl Display for HttpProtocol {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let protocol = match self {
+            HttpProtocol::OnePointOne => "HTTP/1.1",
+        };
+        write!(f, "{protocol}")
+    }
+}
 enum HttpVerb {
     Get,
     Post,
     Put,
+}
+
+impl Display for HttpVerb {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let verb = match self {
+            HttpVerb::Get => "GET",
+            HttpVerb::Post => "POST",
+            HttpVerb::Put => "PUT",
+        };
+        write!(f, "{verb}",)
+    }
+}
+
+impl FromStr for HttpVerb {
+    type Err = String;
+
+    fn from_str(verb: &str) -> Result<Self, Self::Err> {
+        match verb {
+            "GET" => Ok(HttpVerb::Get),
+            "POST" => Ok(HttpVerb::Post),
+            "PUT" => Ok(HttpVerb::Put),
+            _ => Err(format!("cannot covert {verb} to HttpVerb.")),
+        }
+    }
 }
 
 struct HttpResponse {
@@ -84,11 +169,11 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|res| res.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-
     println!("Request: {http_request:#?}");
+    let http_request = HttpRequest::build(http_request).unwrap();
+    print!("{http_request}");
 
     let body = fs::read_to_string("index.html").unwrap();
-
     let response = HttpResponse {
         code: HttpCode::Ok,
         body,
